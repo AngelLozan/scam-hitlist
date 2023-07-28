ae0736ca9bae50d6bc3681a442948f9934f849a2
Scott Lozano <scottloz@protonmail.com>
2023-07-28 15:35:25 +0100

    save changes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: %i[:home :rubric]
+  skip_before_action :authenticate_user!, only: %i[:home]
   helper_method :sort_column, :sort_direction, :sort_column_host
 
   def home

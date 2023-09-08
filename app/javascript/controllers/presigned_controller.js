d0d283d2474d181b32ff0179f48a7eb20605479b
scott lozano <scottloz@protonmail.com>
2023-09-08 23:23:08 +0100

    allows uploads to s3 bucket presigned url

--- app/javascript/controllers/presigned_controller.js
+++ app/javascript/controllers/presigned_controller.js
@@ -1,4 +1,5 @@
 import { Controller } from "@hotwired/stimulus"
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
 
 
 export default class extends Controller {
@@ -23,32 +24,64 @@ export default class extends Controller {
         }
     }
 
-    handleSubmit(event) {
+    put(url, data) {
+        return new Promise((resolve, reject) => {
+            fetch(url, {
+                    method: 'PUT',
+                    headers: {
+                        'Content-Length': new Blob([data]).size.toString(),
+                    },
+                    body: data,
+                })
+                .then((response) => {
+                    if (!response.ok) {
+                        throw new Error('Request failed with status ' + response.status);
+                    }
+                    return response.text();
+                })
+                .then((responseBody) => {
+                    resolve(responseBody);
+                })
+                .catch((error) => {
+                    console.log(error);
+                    reject(error);
+                });
+        });
+    }
+
+    async handleSubmit(event) {
         event.preventDefault();
-        const path = this.formTarget.action
-        const upload_file_url = this.fileUrlTarget.getAttribute("data-url");
+        try {
+            const path = this.formTarget.action
+            const file = this.fileInputTarget.value
+            const upload_file_url = this.fileUrlTarget.getAttribute("data-url");
+            this.fileUrlTarget.value = upload_file_url
+            const formData = await new FormData(this.formTarget);
 
-        this.fileUrlTarget.value = upload_file_url
-        const formData = new FormData(this.formTarget);
+            await formData.set('file_url', upload_file_url);
 
-        formData.set('file_url', upload_file_url);
+            console.log("Calling PUT using presigned URL with client");
+            await this.put(upload_file_url, file);
+            console.log("\nDone. Check your S3 console.");
 
-
-        fetch(path, {
+            let res = await fetch(path, {
                 method: 'POST',
                 body: formData
-            })
-            .then(response => response.json())
-            .then((data) => {
-                if (data.show_url) {
-                    window.location.href = data.show_url;
-                } else {
-                    console.log("Data:", data);
-                }
-            })
-            .catch((error) => {
-                console.error(error);
             });
+            let data = await res.json();
+
+            if (data.show_url) {
+                window.location.href = data.show_url;
+            } else {
+                console.log("Data:", data);
+            }
+
+        } catch (e) {
+            console.log(e);
+        }
+
     }
+
+
 
 }
